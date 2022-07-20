a98d154bbdf23c609243830390884a42d389279b
namnv <namnv2302@gmail.com>
2022-07-20 13:37:01 +0700

    up

--- index.php
+++ index.php
@@ -9,21 +9,20 @@ $collection = new QuestionCollection();
 $collection->fromMdFile('https://raw.githubusercontent.com/lydiahallie/javascript-questions/master/vi-VI/README-vi.md');
 $array = $collection->get();
 
-//print_r($array);
-//print_r($array[0]);
+print_r($array);
+print_r($array[0]);
 
 
 // Lấy ra câu hỏi và đáp án thứ 1
-$answer = explode('####', $array[0]);
+$answer = explode('####', $array[150]);
 array_shift($answer);
 $answer = implode("", $answer);
 
-$question = explode('####', $array[0]);
+$question = explode('####', $array[150]);
 array_pop($question);
 $question = implode("", $question);
 
 
 $item = new Question($question, $answer);
-//echo $item->getAnswer();
+echo $item->getAnswer();
 echo $item->getContent();
-
